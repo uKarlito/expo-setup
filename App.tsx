96a9edbe249d276fa9af8d4991b869c629621e26
uKarlito <dedepimpu@gmail.com>
2026-03-02 20:48:28 -0300

    Criand componente (Hello)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { Text, View } from "react-native";
+import { Hello } from "./components/Hello";
 
 export default function App() {
   return <View>
   <Text>Rélou Woudi!</Text>
+  <Hello />
   </View>;
 }
---
+++ components/Hello.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export const Hello = () => {
+    return <Text>Rélou Woudi!</Text>
+}
